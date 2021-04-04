47feee2ee5ed7f2fb1686bab662151f264a0cfc9
Jirka <vrbka.jirka@gmail.com>
2021-04-04 21:39:24 +0200

    Added configuration

---
+++ src/main/java/cz/idomatojde/InMemoryDatabase.java
@@ -0,0 +1,23 @@
+package cz.idomatojde;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+
+@Configuration
+@EnableJpaRepositories
+public class InMemoryDatabase {
+
+    @Bean
+    public DataSource db(){
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        return db;
+    }
+}
