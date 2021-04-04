d9a29fd420f7926f4ab6521c1e4e5cff477e03d5
Michal-MK <michalhz159@gmail.com>
2021-04-05 00:02:03 +0200

    Fixed JPA - fixed persistence.xml placement => belongs to META-INF, causing the injection to fail - added AssertJ and other logging dependencies to pom.xml - removed unnecessary files / renamed folders to follow lowercase conventions - removed InMemoryDatabase as the PersistenceAppContext uses apache.derby internally (I assume), which is an in-memory database - added tests for environment to assert functional injection

---
+++ src/test/java/cz/idomatojde/EnvironmentTests.java
@@ -0,0 +1,14 @@
+package cz.idomatojde;
+
+import cz.idomatojde.base.TestBase;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EnvironmentTests extends TestBase {
+    @Test
+    void assertEnvironment() {
+        assertThat(em).isNotNull();
+        assertThat(emf).isNotNull();
+    }
+}
