6aedc768eb9a9ff9d41bd628b70192d1269d053b
Michal-MK <michalhz159@gmail.com>
2021-04-05 23:47:03 +0200

    Added Timetable and related entities

--- src/main/java/cz/idomatojde/entity/Timetable.java
+++ src/main/java/cz/idomatojde/entity/Timetable.java
@@ -1,13 +1,33 @@
 package cz.idomatojde.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Timetable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private int week;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "YEAR_VALUE")
+    private int year;
+
+    @OneToMany(mappedBy = "timetable", targetEntity = TimetableEntry.class)
+    private List<TimetableEntry> entries;
+
 
     public void setId(Long id) {
         this.id = id;
@@ -15,5 +35,37 @@ public class Timetable {
 
     public Long getId() {
         return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUserId(User user) {
+        this.user = user;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int yearVal) {
+        this.year = yearVal;
+    }
+
+    public List<TimetableEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<TimetableEntry> entries) {
+        this.entries = entries;
     }
 }
--- src/main/java/cz/idomatojde/entity/TimetableEntry.java
+++ src/main/java/cz/idomatojde/entity/TimetableEntry.java
@@ -1,13 +1,35 @@
 package cz.idomatojde.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class TimetableEntry {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private Timetable timetable;
+
+    @ManyToOne
+    private Offer offer;
+
+    private LocalTime entryStart;
+
+    private Duration length;
+
+    @OneToMany(mappedBy = "timetableEntry", targetEntity = TimetableChatMessage.class)
+    private List<TimetableChatMessage> messages = new ArrayList<>();
 
     public void setId(Long id) {
         this.id = id;
@@ -15,5 +37,45 @@ public class TimetableEntry {
 
     public Long getId() {
         return id;
+    }
+
+    public Timetable getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(Timetable timetable) {
+        this.timetable = timetable;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    public LocalTime getEntryStart() {
+        return entryStart;
+    }
+
+    public void setEntryStart(LocalTime entryStart) {
+        this.entryStart = entryStart;
+    }
+
+    public Duration getLength() {
+        return length;
+    }
+
+    public void setLength(Duration length) {
+        this.length = length;
+    }
+
+    public List<TimetableChatMessage> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<TimetableChatMessage> messages) {
+        this.messages = messages;
     }
 }
