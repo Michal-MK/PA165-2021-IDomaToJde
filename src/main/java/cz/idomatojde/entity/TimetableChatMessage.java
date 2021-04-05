d45a25cb72475c649984d53808111721ed0845a3
Michal-MK <michalhz159@gmail.com>
2021-04-05 23:47:03 +0200

    Added Timetable and related entities

--- src/main/java/cz/idomatojde/entity/TimetableChatMessage.java
+++ src/main/java/cz/idomatojde/entity/TimetableChatMessage.java
@@ -1,13 +1,25 @@
 package cz.idomatojde.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class TimetableChatMessage {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private TimetableEntry timetableEntry;
+
+    private String text;
 
     public void setId(Long id) {
         this.id = id;
@@ -15,5 +27,29 @@ public class TimetableChatMessage {
 
     public Long getId() {
         return id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public TimetableEntry getTimetableEntry() {
+        return timetableEntry;
+    }
+
+    public void setTimetableEntryId(TimetableEntry timetableEntry) {
+        this.timetableEntry = timetableEntry;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
