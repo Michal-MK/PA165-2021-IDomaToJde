e83166216b369073891b536facc0526f21b462d1
Ondřej Urbanovský <shatieu@gmail.com>
2021-04-05 20:29:55 +0200

    getters setters

--- src/main/java/cz/idomatojde/entity/User.java
+++ src/main/java/cz/idomatojde/entity/User.java
@@ -48,4 +48,99 @@ public class User {
     @NotNull
     private boolean wantsAdvertisment;
 
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassHash() {
+        return passHash;
+    }
+
+    public void setPassHash(String passHash) {
+        this.passHash = passHash;
+    }
+
+    public String getPassSalt() {
+        return passSalt;
+    }
+
+    public void setPassSalt(String passSalt) {
+        this.passSalt = passSalt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+
+    public Integer getBonusCredits() {
+        return bonusCredits;
+    }
+
+    public void setBonusCredits(Integer bonusCredits) {
+        this.bonusCredits = bonusCredits;
+    }
+
+    public HashMap<Long, Offer> getOfferHashMap() {
+        return offerHashMap;
+    }
+
+    public void setOfferHashMap(HashMap<Long, Offer> offerHashMap) {
+        this.offerHashMap = offerHashMap;
+    }
+
+    public boolean isWantsAdvertisment() {
+        return wantsAdvertisment;
+    }
+
+    public void setWantsAdvertisment(boolean wantsAdvertisment) {
+        this.wantsAdvertisment = wantsAdvertisment;
+    }
 }
