c345e563ab902f9ae8061b17704cc49e22476145
Michal-MK <michalhz159@gmail.com>
2021-04-06 15:13:59 +0200

    Added more functionality to Timetables

--- src/main/java/cz/idomatojde/dao/TimetableDAO.java
+++ src/main/java/cz/idomatojde/dao/TimetableDAO.java
@@ -1,18 +1,27 @@
 package cz.idomatojde.dao;
 
+import cz.idomatojde.entity.Offer;
 import cz.idomatojde.entity.Timetable;
 import cz.idomatojde.entity.TimetableEntry;
 import cz.idomatojde.entity.User;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 
 
 public interface TimetableDAO {
     Timetable createTimetable(User user, int year, int week);
 
+    TimetableEntry createEntry(Timetable timetable, Offer offer, LocalTime start, Duration duration);
+
+    void removeEntry(TimetableEntry entry);
+
     Timetable getTimetable(Long timetableId);
 
-    Timetable getTimetable(int year, int week);
+    Timetable getTimetable(User user, int year, int week);
+
+    Timetable getTimetableForCurrentWeek(User user);
 
     List<TimetableEntry> getAllTimetableEntries(Long timetableId);
 }
--- src/main/java/cz/idomatojde/dao/TimetableDAOImpl.java
+++ src/main/java/cz/idomatojde/dao/TimetableDAOImpl.java
@@ -1,5 +1,6 @@
 package cz.idomatojde.dao;
 
+import cz.idomatojde.entity.Offer;
 import cz.idomatojde.entity.Timetable;
 import cz.idomatojde.entity.TimetableEntry;
 import cz.idomatojde.entity.User;
@@ -8,6 +9,10 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 @Repository
@@ -29,6 +34,27 @@ public class TimetableDAOImpl implements TimetableDAO {
     }
 
     @Override
+    @Transactional
+    public TimetableEntry createEntry(Timetable timetable, Offer offer, LocalTime start, Duration duration) {
+        TimetableEntry entry = new TimetableEntry();
+        entry.setEntryStart(start);
+        entry.setTimetable(timetable);
+        entry.setLength(duration);
+        entry.setOffer(offer);
+
+        em.persist(entry);
+
+        timetable.getEntries().add(entry);
+        return entry;
+    }
+
+    @Override
+    @Transactional
+    public void removeEntry(TimetableEntry entry) {
+        em.remove(entry);
+    }
+
+    @Override
     public Timetable getTimetable(Long timetableId) {
         return em.createQuery("select t from Timetable t where t.id = :id", Timetable.class)
                 .setParameter("id", timetableId)
@@ -36,16 +62,25 @@ public class TimetableDAOImpl implements TimetableDAO {
     }
 
     @Override
-    public Timetable getTimetable(int year, int week) {
-        return em.createQuery("select t from Timetable t where t.year = :y and t.week = :w", Timetable.class)
+    public Timetable getTimetable(User user, int year, int week) {
+        return em.createQuery("select t from Timetable t where" +
+                " t.user = :user and t.year = :y and t.week = :w",
+                Timetable.class)
+                .setParameter("user", user)
                 .setParameter("y", year)
                 .setParameter("w", week)
                 .getSingleResult();
     }
 
+    public Timetable getTimetableForCurrentWeek(User user) {
+        int week = LocalDate.now().get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+        int year = LocalDate.now().getYear();
+        return getTimetable(user, year, week);
+    }
+
     @Override
     public List<TimetableEntry> getAllTimetableEntries(Long timetableId) {
-        return em.createQuery("select t from Timetable t where t.id = :id", Timetable.class)
+        return em.createQuery("select t from Timetable t join fetch t.entries where t.id = :id", Timetable.class)
                 .setParameter("id", timetableId)
                 .getSingleResult()
                 .getEntries();
