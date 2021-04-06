bf8676afbeb7e95090501d588f89f22d080a2073
Michal-MK <michalhz159@gmail.com>
2021-04-06 13:41:56 +0200

    Fixes and dependencies - added org.sonatype as it holds the official @Inject annotation - removed PersistenceContextType.EXTENDED from TestBase due to unintended behaviour - escaped User entity table name as it apparently is a reserved word

--- src/test/java/cz/idomatojde/base/TestBase.java
+++ src/test/java/cz/idomatojde/base/TestBase.java
@@ -7,13 +7,12 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
 import javax.persistence.PersistenceUnit;
 
 @ContextConfiguration(classes = PersistenceAppContext.class)
 public class TestBase extends AbstractTestNGSpringContextTests {
 
-    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    @PersistenceContext
     protected EntityManager em;
 
     @PersistenceUnit
