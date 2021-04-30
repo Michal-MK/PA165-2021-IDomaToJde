38c4e12ea133510694e3dae2724f43af6f88071c
Jirka <vrbka.jirka@gmail.com>
2021-04-30 03:22:47 +0200

    Author

--- business/src/main/java/cz/idomatojde/services/OfferServiceImpl.java
+++ business/src/main/java/cz/idomatojde/services/OfferServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * @author Jiri Vrbka
+ */
 @Service
 public class OfferServiceImpl extends BaseServiceImpl<Offer> implements OfferService {
 
