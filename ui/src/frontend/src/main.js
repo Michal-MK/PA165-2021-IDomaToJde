199d504d05e5d05bada1679b4081cbfd47cc7150
Jirka <vrbka.jirka@gmail.com>
2021-06-06 23:44:38 +0200

    removed comments

--- ui/src/frontend/src/main.js
+++ ui/src/frontend/src/main.js
@@ -3,101 +3,18 @@ import App from './App.vue'
 import VueCookies from 'vue3-cookies'
 import { createStore } from 'vuex'
 import apiModule from './modules/apiModule'
+import userModule from "@/modules/userModule";
 
 
 let app = createApp(App);
 app.use(VueCookies);
 
 
-//
-// const apiModule = {
-//     state: {
-//         tokenName: 'token',
-//         isAuthenticated: false
-//     },
-//
-//     computed: {
-//       getToken(){
-//           return this.$cookies.get(this.tokenName);
-//       }
-//
-//
-//     },
-//
-//     getters: {
-//         dummy(){
-//             return "ano";
-//         },
-//
-//
-//         isAuth(){
-//             let token = this.getToken();
-//
-//             if(this.stringEmpty(token)) {
-//                 this.isAuthenticated = false
-//                 return false;
-//             }
-//
-//
-//
-//
-//         },
-//
-//         async AuthUser(){
-//             let token = this.getToken();
-//
-//             if(this.stringEmpty(token)){
-//                 console.log("Unauthenticated user");
-//                 this.isAuthenticated = false;
-//             }else{
-//                 console.log("Authenticated user");
-//                 if(this.isEmpty(this.user)) {
-//                     this.user = await this.fetchApiUser(token);
-//
-//                 }
-//
-//                 console.log("Loaded user: " + JSON.stringify(this.user));
-//                 this.isAuthenticated = true;
-//             }
-//         },
-//
-//         async fetchApiUser(token){
-//             const response = await fetch("api/auth/authenticate" , {
-//                 method: 'POST',
-//                 headers: {
-//                     "token" : token
-//                 }
-//             });
-//
-//             const data = await response.json();
-//             console.log("Fetched by token: " + JSON.stringify(data));
-//             return data;
-//         },
-//
-//
-//         stringEmpty: () => (string) => {
-//             return string == null || string.length === 0;
-//         },
-//
-//
-//     },
-//
-//
-// }
-//
-//
-// const store = createStore({
-//     modules: {
-//         api: apiModule,
-//     }
-// })
-
-
-
 // Create a new store instance.
 const store = createStore({
     modules: {
-        api: apiModule
+        api: apiModule,
+        user: userModule
     }
 })
 
