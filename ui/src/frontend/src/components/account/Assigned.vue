9e0d51048a27b9f3f3d0f8472ad5e9a321f7f51b
Jirka <vrbka.jirka@gmail.com>
2021-06-06 23:45:42 +0200

    component for subscriptions

---
+++ ui/src/frontend/src/components/account/Assigned.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="loading">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+  <div v-else>
+    <tr v-for="row in getSubscribed()" :key="row.id">
+      <button class="btn btn-outline-primary m-1">
+        <td>{{ row.title }}</td>
+      </button>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Assigned",
+
+
+  data() {
+    return {
+      loading: false
+    }
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    getSubscribed() {
+      return this.$store.getters.getSubscribed;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
